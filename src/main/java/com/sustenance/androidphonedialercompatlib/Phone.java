9618cb1dde3260242df704b05be0e12192325a2d
Sustenance <marinehacker202@gmail.com>
2015-08-24 19:01:31 -0400

    Got rid of the many dialer methods, made a generic dial method. Expanded the contents of the Phone enum Added Hangouts Dialer and Nextplus

--- src/main/java/com/sustenance/androidphonedialercompatlib/Phone.java
+++ src/main/java/com/sustenance/androidphonedialercompatlib/Phone.java
@@ -1,25 +1,34 @@
 package com.sustenance.androidphonedialercompatlib;
 
+import android.content.Intent;
+
 /**
  * Created by Sustenance on 8/23/15.
  */
 public enum Phone {
-    DEFAULT("", ""),
-    SKYPE("com.skype.raider", "com.skype.raider.Main"),
-    TALKATONE("com.talkatone.android", "com.talkatone.android.ui.launcher.DialInterceptor"),
-    VIBER("com.viber.voip", "com.viber.voip.WelcomeActivity"),
-    GROOVE_IP("com.gvoip", "com.gvoip.ui.SwipeTabsActivity"),
-    GROOVE_IP_LITE("com.snrblabs.grooveip", "com.gvoip.ui.SwipeTabsActivity"),
-    TEXTPLUS("com.gogii.textplus", "com.gogii.textplus.view.VoiceIntentActivity"),
-    VONAGE("com.vonage.TimeToCall", "com.vonage.TimeToCall.Activities.CallPrilegedHandlerActivity"),
-    MAGICJACK("com.magicjack", "com.magicjack.ContactsIntegrationActivity");
+    DEFAULT("", "", Intent.ACTION_CALL, "tel:1"),
+    GROOVE_IP("com.gvoip", "com.gvoip.ui.SwipeTabsActivity", Intent.ACTION_DIAL,"tel:+1"),
+    GROOVE_IP_LITE("com.snrblabs.grooveip", "com.gvoip.ui.SwipeTabsActivity", Intent.ACTION_DIAL,"tel:+1"),
+    HANGOUTS_DIAL("com.google.android.apps.hangoutsdialer", "com.google.android.apps.hangoutsdialer.app.LaunchActivity", Intent.ACTION_DIAL, "tel:+1"),
+    //LINE("jp.naver.line.android", "", Intent.ACTION_CALL, "tel:+1"),
+    MAGICJACK("com.magicjack", "com.magicjack.ContactsIntegrationActivity", Intent.ACTION_CALL,"tel:+1"),
+    NEXTPLUS("me.nextplus.smsfreetext.phonecalls", "com.nextplus.android.activity.DialerActivity", Intent.ACTION_DIAL, "tel:+1"),
+    SKYPE("com.skype.raider", "com.skype.raider.Main", Intent.ACTION_CALL, "skype:+1"),
+    TALKATONE("com.talkatone.android", "com.talkatone.android.ui.launcher.OutgoingCallInterceptor" ,Intent.ACTION_CALL,"tel:1"),
+    TEXTPLUS("com.gogii.textplus", "com.gogii.textplus.view.VoiceIntentActivity", Intent.ACTION_CALL,"tel:+1"),
+    VONAGE("com.vonage.TimeToCall", "com.vonage.TimeToCall.Activities.CallPrilegedHandlerActivity", "android.intent.action.CALL_PRIVILEGED","tel:+1"),
+    VIBER("com.viber.voip", "com.viber.voip.WelcomeActivity", Intent.ACTION_DIAL,"tel:");
 
     public String packageName;
     public String activityName;
+    public String intentAction;
+    public String uriPrefix;
 
-    Phone(String packageName, String activityName) {
+    Phone(String packageName, String activityName, String intentAction, String uriPrefix) {
         this.packageName = packageName;
         this.activityName = activityName;
+        this.intentAction = intentAction;
+        this.uriPrefix = uriPrefix;
     }
 
 }
