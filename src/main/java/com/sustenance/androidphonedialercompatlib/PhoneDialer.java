64f07cd8b66872695857b5f0e651d552526261d6
Sustenance <tony@sustenance.dev>
2015-08-24 19:01:31 -0400

    Got rid of the many dialer methods, made a generic dial method. Expanded the contents of the Phone enum Added Hangouts Dialer and Nextplus

--- src/main/java/com/sustenance/androidphonedialercompatlib/PhoneDialer.java
+++ src/main/java/com/sustenance/androidphonedialercompatlib/PhoneDialer.java
@@ -30,7 +30,7 @@ public class PhoneDialer {
 
     public PhoneDialer(Context context){
         this.context = context;
-        this.numApps = 0;
+        numApps = 0;
         this.installedPhones = new ArrayList<String>();
         this.installedPhonesByName = new ArrayList<String>();
         scanApps();
@@ -76,42 +76,50 @@ public class PhoneDialer {
     }
 
 
+    /**
+     *
+     * @param phoneNumber The phone numbe to dial
+     * @param phone The selected phone app to dial with
+     * @return True if success
+     */
     public boolean dial(String phoneNumber, Phone phone) {
-        if (phoneNumber.length() == 10){
-            switch (phone) {
-                case GROOVE_IP:
-                    groove(phoneNumber);
-                    break;
-                case GROOVE_IP_LITE:
-                    grooveLite(phoneNumber);
-                    break;
-                case MAGICJACK:
-                    magicJack(phoneNumber);
-                    break;
-                case SKYPE:
-                    skype(phoneNumber);
-                    break;
-                case TALKATONE:
-                    talkatone(phoneNumber);
-                    break;
-                case TEXTPLUS:
-                    textPlus(phoneNumber);
-                    break;
-                case VIBER:
-                    viber(phoneNumber);
-                    break;
-                case VONAGE:
-                    vonage(phoneNumber);
-                    break;
-                case DEFAULT:
-                    defaultDialer(phoneNumber);
-                    break;
-                default:
-                    Log.d("PHONEDIALLIB", "Supplied Unsupported Phone: " + phone.packageName);
-                    break;
+        phoneNumber = trimPhoneNumberLength(phoneNumber);
+        if(phoneNumber.length() == 10){
+            try {
+                Intent callIntent = new Intent(phone.intentAction, Uri.parse(phone.uriPrefix + phoneNumber));
+                if(!phone.packageName.equals("") && !phone.activityName.equals("")) {
+                    callIntent.setComponent(new ComponentName(phone.packageName, phone.activityName));
+                }
+                callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(callIntent);
+            } catch (ActivityNotFoundException e) {
+                Log.e("DIAL LIB", phone.packageName + " call failed", e);
+                return false;
             }
+            return true;
         }
+
         return false;
+    }
+
+    /**
+     * Ensures that the supplied phone number is trimmed of dashes and
+     * is only Ten digits long
+     * @param phoneNumber A phone number
+     * @return A phone number in a dialable format (XXXXXXXXXX)
+     */
+    public String trimPhoneNumberLength(String phoneNumber) {
+        phoneNumber = phoneNumber.replaceAll("-","");
+
+        int length = phoneNumber.length();
+        if(length == 10){
+            return phoneNumber;
+        }else if(length == 11 && phoneNumber.substring(0,1).equals("1")){
+            return phoneNumber.substring(1);
+        }else{
+            Log.d("Incorrect number", "number supplied was incorrect format: " + phoneNumber);
+            return phoneNumber;
+        }
     }
 
     /**
@@ -161,138 +169,7 @@ public class PhoneDialer {
             installedPhones.add("Default");
             installedPhonesByName.add("Default");
             java.util.Collections.sort(installedPhones);
-            //Log.d("INSTALLED PHONES", installedPhones.toString());
         }
-    }
-
-    public boolean defaultDialer(String number) {
-        try {
-            Intent intentGo;
-            if (number.length() == 10) {
-                intentGo = new Intent(Intent.ACTION_CALL);
-            } else {
-                intentGo = new Intent(Intent.ACTION_VIEW);
-            }
-            intentGo.setData(Uri.parse("tel:1" + number));
-            intentGo.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(intentGo);
-            return true;
-        } catch (ActivityNotFoundException e) {
-            Log.e("DEFAULT CALL", "Default Dialer call failed!", e);
-            return false;
-        }
-
-    }
-
-    public boolean skype(String number) {
-        try {
-            Intent callSkype = new Intent(Intent.ACTION_CALL, Uri.parse("skype:+1" + number));
-            callSkype.setComponent(new ComponentName("com.skype.raider", "com.skype.raider.Main"));
-            callSkype.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(callSkype);
-        } catch (ActivityNotFoundException e) {
-            Log.e("SKYPE CALL", "Skype Call Failed", e);
-            return false;
-        }
-        return true;
-    }
-
-    public boolean textPlus(String number) {
-        try {
-            Intent callTextPlus = new Intent(Intent.ACTION_CALL, Uri.parse("tel:+1" + number));
-            callTextPlus.setComponent(new ComponentName("com.gogii.textplus", "com.gogii.textplus.view.VoiceIntentActivity"));
-            callTextPlus.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(callTextPlus);
-        } catch (ActivityNotFoundException e) {
-            Log.e("TEXTPLUS", "textPlus call failed", e);
-            return false;
-        }
-        return true;
-    }
-
-    public boolean talkatone(String number) {
-        try {
-            Intent callTalkatone = new Intent(Intent.ACTION_CALL, Uri.parse("tel:+1" + number));
-            callTalkatone.setComponent(new ComponentName("com.talkatone.android", "com.talkatone.android.ui.launcher.OutgoingCallInterceptor"));
-            callTalkatone.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(callTalkatone);
-        } catch (ActivityNotFoundException e) {
-            Log.e("TKTN CALL", "TKTN call Failed", e);
-            return false;
-        }
-        return true;
-    }
-
-    //Viber doesn't allow starting calls from external applications
-    //opens Viber app with number prepopulated, ready for calling
-    public boolean viber(String number) {
-        try {
-            Intent callViber = new Intent(Intent.ACTION_VIEW);
-            callViber.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            Uri uri = Uri.parse("tel:" + Uri.encode(number));
-            callViber.setClassName("com.viber.voip", "com.viber.voip.WelcomeActivity");
-            callViber.setData(uri);
-            context.startActivity(callViber);
-        } catch (ActivityNotFoundException e) {
-            Log.e("VIBER CALL", "Viber call Failed", e);
-            return false;
-        }
-        return true;
-    }
-
-    public boolean groove(String number) {
-        try {
-            Intent callGroove = new Intent("android.intent.action.DIAL");
-            callGroove.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            callGroove.setData(Uri.parse("tel:+1" + number));
-            callGroove.setClassName("com.gvoip", "com.gvoip.ui.SwipeTabsActivity");
-            context.startActivity(callGroove);
-        } catch (ActivityNotFoundException e) {
-            Log.e("GROOVE CALL", "Groove call failed", e);
-            return false;
-        }
-        return true;
-    }
-
-    public boolean grooveLite(String number) {
-        try {
-            Intent callGroove = new Intent("android.intent.action.DIAL");
-            callGroove.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            callGroove.setData(Uri.parse("tel:+1" + number));
-            callGroove.setClassName("com.snrblabs.grooveip", "com.gvoip.ui.SwipeTabsActivity");
-            context.startActivity(callGroove);
-        } catch (ActivityNotFoundException e) {
-            Log.e("GROOVE Lite CALL", "Groove lite call failed", e);
-            return false;
-        }
-        return true;
-    }
-
-    public boolean vonage(String number) {
-        try {
-            Intent callVonage = new Intent("android.intent.action.CALL_PRIVILEGED");
-            callVonage.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            callVonage.setData(Uri.parse("tel:+1" + number));
-            callVonage.setComponent(new ComponentName("com.vonage.TimeToCall", "com.vonage.TimeToCall.Activities.CallPrilegedHandlerActivity"));
-            context.startActivity(callVonage);
-        } catch (ActivityNotFoundException e) {
-            Log.e("VONAGE CALL", "Vonage call failed", e);
-            return false;
-        }
-        return true;
-    }
-
-    public boolean magicJack(String number) {
-        try {
-            Intent callMagic = new Intent("android.intent.action.CALL", Uri.parse("tel:+1" + number));
-            callMagic.setComponent(new ComponentName("com.magicjack", "com.magicjack.ContactsIntegrationActivity"));
-            callMagic.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(callMagic);
-        } catch (ActivityNotFoundException e) {
-            Log.e("MAGIC CALL", "magicJack call failed", e);
-            return false;
-        }
-        return true;
     }
 
 }
